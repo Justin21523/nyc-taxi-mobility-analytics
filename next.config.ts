d1cda353da5a8d8f5a247d2659155d8539d1befa
justin21523 <b0979865617@gmail.com>
2026-06-27 01:55:18 +0800

    Align portfolio route trailing slash

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH?.replace(/\/$/, "");
 
 const nextConfig: NextConfig = {
   basePath: basePath || undefined,
+  trailingSlash: true,
   serverExternalPackages: ["@duckdb/node-api"],
 };
 
